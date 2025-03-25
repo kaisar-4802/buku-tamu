9b051468b187266d37e1f825b37c1fa3d8e1d865
kaisar rozan b <kaisarrozanbasalika891@gmail.com>
2025-03-25 10:12:40 +0700

    Add files via upload

--- insert_data.php
+++ insert_data.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $servername = "localhost";
 $username = "root";
@@ -30,41 +29,3 @@ if ($conn->query($sql) === TRUE) {
 
 $conn->close();
 ?>
-=======
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "buku_tamu";
-
-// Koneksi ke database
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Cek koneksi
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
-}
-
-// Insert data ke tabel keperluan
-$sql = "INSERT INTO keperluan (deskripsi) VALUES 
-        ('Meeting'), ('Interview'), ('Kunjungan')";
-if ($conn->query($sql) === TRUE) {
-    echo "Data keperluan berhasil ditambahkan.<br>";
-} else {
-    echo "Error: " . $conn->error;
-}
-
-// Insert data ke tabel tamu
-$sql = "INSERT INTO tamu (nama, email, pesan, keperluan_id) VALUES 
-        ('Abyaz', 'abyaz@example.com', 'Saya ingin bertemu dengan manajer.', 1),
-        ('Kaisar', 'kaisar@example.com', 'Saya datang untuk wawancara.', 2)";
-if ($conn->query($sql) === TRUE) {
-    echo "Data tamu berhasil ditambahkan.<br>";
-} else {
-    echo "Error: " . $conn->error;
-}
-
-// Tutup koneksi
-$conn->close();
-?>
->>>>>>> 50aefcf (Menambahkan file create_db.php dan insert_data.php)
